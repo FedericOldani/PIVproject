15e7cabe07b04d924525fa8ae9c65482db780fa1
andreguerreiiro <andree.guerreiiro@gmail.com>
2017-12-06 18:44:09 +0000

    Create Variables

--- remove_bg.m
+++ remove_bg.m
@@ -1,7 +1,7 @@
 function objects=remove_bg( depth, bg)
-    absimage=abs((double(depth)/1000-bg))>0.1; %pick objects which differ at least 25cm from bg
-    absimage(depth>2000)=0;
-    absimage=medfilt2(absimage);
-    objects=bwareaopen(absimage,2000); %pick objects with more than 500px
+    absimage=abs((double(depth)/1000-bg))>0.05; %pick objects which differ at least 5cm from bg
+    absimage(depth>2000)=0; %more than 2 meters
+    %absimage=medfilt2(absimage);
+    objects=bwareaopen(absimage,900); %pick objects with more than 900px
     
     %objects=absimage;
