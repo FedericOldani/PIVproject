abbb0005088463217b0c7ad2d98d6c192909f4dc
andreguerreiiro <andree.guerreiiro@gmail.com>
2017-12-16 16:31:25 +0000

    testing variables

--- track3D_testAndre.m
+++ track3D_testAndre.m
@@ -22,6 +22,7 @@ acceptpointsArray = [700 800 900 1000 1100 1200 1300 1400 1500 1600 1700];
 waitSeconds = 7; %See the image for 7 seconds
 
 
+test = [1 1 1 2 2 2 1 3 1 2 3 3 2 3 3 2 2 2 3 2];
 
 bg1=get_bg(imgseq1);
 bg2=get_bg(imgseq2);
@@ -31,135 +32,145 @@ objects=struct('X',{[]},'Y',{[]},'Z',{[]},'frames_tracked',{});
 
 %eliminate bg for all images in dataset
 
-nrObjs = zeros(11,11,11,11,11,11,length(imgseq1));
+nrObjs = zeros(11,11,11,11,11,length(imgseq1)+1);
 
-for a=farawayArray
-    for b=objsizeArray
-       for c=bgdistArray
-          for d=acceptholesArray
-            for e=nearpointsArray
-                for f=acceptpointsArray
-                   for i=1:length(imgseq1)
-%       i=1; 
-                        new_obj=struct('X',{[]},'Y',{[]},'Z',{[]},'frames_tracked',{},'hue1',{},'sat1',{});
-                        close all;
-                        fprintf('\n%d...',i);
-                        % ----------------------  get objects in image1   ----------------------
+for a=1:length(farawayArray)
+    a
+    for b=1:length(objsizeArray)
+        b
+       for c=1:length(bgdistArray)
+           c
+          % d=1:length(acceptholesArray)
+             % d
+            for e=1:length(nearpointsArray)
+                e
+                for f=1:length(acceptpointsArray)
+                    f
+                   for i=1:(length(imgseq1)+1)
+%       i=1;    
+                        if(i == length(imgseq1)+1)
+                         
+                        else
+                            new_obj=struct('X',{[]},'Y',{[]},'Z',{[]},'frames_tracked',{},'hue1',{},'sat1',{});
+                            close all;
+                            %fprintf('\n%d...',i);
+                            % ----------------------  get objects in image1   ----------------------
 
-                        load(imgseq1(i).depth);
-                        dep1=depth_array;
-                        obj1=remove_bg(dep1, bg1, bgdist, farAwayObj, objsize);
-                        %obj1=bwpropfilt(obj1,'EulerNumber',[-30 1]);%remove regions with many holes    
+                            load(imgseq1(i).depth);
+                            dep1=depth_array;
+                            obj1=remove_bg(dep1, bg1, bgdistArray(c), farawayArray(a), objsizeArray(b));
+                            %obj1=bwpropfilt(obj1,'EulerNumber',[-30 1]);%remove regions with many holes    
 
-                        [L1, num1]=bwlabel(obj1,8);
+                            [L1, num1]=bwlabel(obj1,8);
 
-                    %      se = strel('disk',10);
-                    %      L1=imopen(L1,se);
-
-
-                        % ----------------------  get objects in image2   ----------------------
-
-                        load(imgseq2(i).depth);
-                        dep2=depth_array;
-
-                        obj2=remove_bg(dep2,bg2, bgdist, farAwayObj, objsize);
-                        %obj2=bwpropfilt(obj2,'EulerNumber',[-30 1]);%remove regions with many holes
-
-                        [L2, num2]=bwlabel(obj2,8);
-
-                    %      L2=imopen(L2,se);
-
-                        im1=imread(imgseq1(i).rgb);
-                        im2=imread(imgseq2(i).rgb);
+                        %      se = strel('disk',10);
+                        %      L1=imopen(L1,se);
 
 
-                        dep1(find(dep1>farAwayObj))=0;%eliminate too far objects 2 meters
-                        dep2(find(dep2>farAwayObj))=0;%eliminate too far objects
+                            % ----------------------  get objects in image2   ----------------------
 
-                        %get coordinate in a unique reference system
-                        [xyz1,rgbd1]=getCoordinate(dep1,im1,cam1toW,cam_params);
-                        [xyz21,rgbd2]=getCoordinate(dep2,im2,cam2toW,cam_params);
+                            load(imgseq2(i).depth);
+                            dep2=depth_array;
 
-                        pc1=getPointCloud(xyz1,rgbd1);
-                        pc2=getPointCloud(xyz21,rgbd2);
+                            obj2=remove_bg(dep2,bg2, bgdistArray(c), farawayArray(a), objsizeArray(b));
+                            %obj2=bwpropfilt(obj2,'EulerNumber',[-30 1]);%remove regions with many holes
 
-                        %im1
-                        hsv=rgb2hsv(im1);
-                        hsv=reshape(hsv,[480*640 3]);
-                        hue=hsv(:,1);
-                        sat=hsv(:,2);
-                        hue1=hue(L1~=0);
-                        sat1=sat(L1~=0);
+                            [L2, num2]=bwlabel(obj2,8);
 
-                        %im2
-                        hsv=rgb2hsv(im2);
-                        hsv=reshape(hsv,[480*640 3]);
-                        hue=hsv(:,1);
-                        sat=hsv(:,2);
-                        hue2=hue(L2~=0);
-                        sat2=sat(L2~=0);
+                        %      L2=imopen(L2,se);
+
+                            im1=imread(imgseq1(i).rgb);
+                            im2=imread(imgseq2(i).rgb);
 
 
-                        %
+                            dep1(find(dep1>farawayArray(a)))=0;%eliminate too far objects 2 meters
+                            dep2(find(dep2>farawayArray(a)))=0;%eliminate too far objects
+
+                            %get coordinate in a unique reference system
+                            [xyz1,rgbd1]=getCoordinate(dep1,im1,cam1toW,cam_params);
+                            [xyz21,rgbd2]=getCoordinate(dep2,im2,cam2toW,cam_params);
+
+                            pc1=getPointCloud(xyz1,rgbd1);
+                            pc2=getPointCloud(xyz21,rgbd2);
+
+                            %im1
+                            hsv=rgb2hsv(im1);
+                            hsv=reshape(hsv,[480*640 3]);
+                            hue=hsv(:,1);
+                            sat=hsv(:,2);
+                            hue1=hue(L1~=0);
+                            sat1=sat(L1~=0);
+
+                            %im2
+                            hsv=rgb2hsv(im2);
+                            hsv=reshape(hsv,[480*640 3]);
+                            hue=hsv(:,1);
+                            sat=hsv(:,2);
+                            hue2=hue(L2~=0);
+                            sat2=sat(L2~=0);
 
 
-                        obj3d_1=getObj3d(xyz1,L1);
-                        obj3d_2=getObj3d(xyz21,L2);
-
-                        obj3d_1=xyz1(find(L1~=0),:);
-                        [obj3d_1,r1] = unique(obj3d_1,'rows');%optimization, remove rows with duplicated values
-                        hue1=hue1(r1);
-                        sat1=sat1(r1);
-
-                        obj3d_2=xyz21(find(L2~=0),:);
-                        [obj3d_2,r2] = unique(obj3d_2,'rows');%optimization, remove rows with duplicated values
-                        hue2=hue2(r2);
-                        sat2=sat2(r2);
-
-                        obj3d=[obj3d_1; obj3d_2]; %put all points coordinates together
-                        hue=[hue1;hue2];
-                        sat=[sat1;sat2];
-                        %
+                            %
 
 
-                        [kn,D] = knnsearch(obj3d,obj3d,'k',nearPoints);%default 10; more than 50 points together
+                            obj3d_1=getObj3d(xyz1,L1);
+                            obj3d_2=getObj3d(xyz21,L2);
+
+                            obj3d_1=xyz1(find(L1~=0),:);
+                            [obj3d_1,r1] = unique(obj3d_1,'rows');%optimization, remove rows with duplicated values
+                            hue1=hue1(r1);
+                            sat1=sat1(r1);
+
+                            obj3d_2=xyz21(find(L2~=0),:);
+                            [obj3d_2,r2] = unique(obj3d_2,'rows');%optimization, remove rows with duplicated values
+                            hue2=hue2(r2);
+                            sat2=sat2(r2);
+
+                            obj3d=[obj3d_1; obj3d_2]; %put all points coordinates together
+                            hue=[hue1;hue2];
+                            sat=[sat1;sat2];
+                            %
 
 
-                        idx=0;
-                        if ~isempty(kn)
-                            DG=sparse(length(kn(:,1)),length(kn(:,1)),0);
-                            for h=2:length(kn(1,:))
-                                DG=DG+sparse(kn(:,1),kn(:,h),D(:,h)<0.1,length(kn(:,1)),length(kn(:,1))); %up to 15cm
-                            end
-                            G=graph(DG,'lower');
-                            bins = conncomp(G);
-                            idx=bins;
-                        end
+                            [kn,D] = knnsearch(obj3d,obj3d,'k',nearpointsArray(e));%default 10; more than 50 points together
 
 
-                        j=0;%index to save 'new_obj'
-                        pcshow(pcmerge(pc1,pc2,0.001));
-
-                        for n=1:max(idx) %num of objects
-
-                            hold on;
-                            if length(find(idx==n))>accept3dPoints %more than 1000 3d points
-                                j=j+1;
-                                %fprintf("printing>");
-                                nrObjs{a,b,c,d,e,f,i} = nrObjs{a,b,c,d,e,f,i}+1; 
+                            idx=0;
+                            if ~isempty(kn)
+                                DG=sparse(length(kn(:,1)),length(kn(:,1)),0);
+                                for h=2:length(kn(1,:))
+                                    DG=DG+sparse(kn(:,1),kn(:,h),D(:,h)<0.1,length(kn(:,1)),length(kn(:,1))); %up to 15cm
+                                end
+                                G=graph(DG,'lower');
+                                bins = conncomp(G);
+                                idx=bins;
                             end
 
 
+                            j=0;%index to save 'new_obj'
+                            %pcshow(pcmerge(pc1,pc2,0.001));
+
+                            for n=1:max(idx) %num of objects
+
+                                hold on;
+                                if length(find(idx==n))>acceptpointsArray(f) %more than 1000 3d points
+                                    j=j+1;
+                                    %fprintf("printing>");
+                                    nrObjs(a,b,c,e,f,i) = nrObjs(a,b,c,e,f,i)+1; 
+                                end
+
+
+                            end
+                               if(nrObjs(a,b,c,e,f,i) == test(i))
+                                    nrObjs(a,b,c,e,f,length(imgseq1)+1) = nrObjs(a,b,c,e,f,length(imgseq1)+1) + 1;
+                                end
+                            
                         end
-                        if(nrObjs{a,b,c,d,e,f,i} > 0)
-                            disp(nrObjs{a,b,c,d,e,f,i});
-                        end
-   
-                    end
-    
+                   end
+                    
                 end
             end
-          end
+          
        end
     end
 end
